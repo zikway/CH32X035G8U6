004d2794d92eb7733856e014a9141d1b9a095abb
xiaoyiyy123 <2916244229@qq.com>
2024-01-18 20:24:51 +0800

    K001_V3218_2024-01-18测试固件 1.添加动态修改回报率 2.调整马达效果

--- User/sdk_api/hw_config.h
+++ User/sdk_api/hw_config.h
@@ -102,6 +102,7 @@ ________________________________________________________________________________
         #define APP_TRIGGER_MIN_R                   (250)
         #define APP_TRIGGER_CAL_DEADZONE            5
         #define APP_TRIGGER_CAL_SIDE_DEADZONE       14
+        #define HW_PWM_FRENQUENCY                   10000
 
         #define HW_MOTOR_ENABLED                   1
         #define JOYSTICK_CAl_END_KEY                (HW_KEY_START | HW_KEY_SELECT)
@@ -113,7 +114,7 @@ ________________________________________________________________________________
 
         #define DEFAULT_MODEL						"ZJ-K001"
         #define DEFAULT_NAME			            "K001"
-        #define SW_VERSION                          0x17
+        #define SW_VERSION                          0x18
     #endif
 #endif
 /*******************************************************************************************************************
