31305fea73aa1a164792cfd42623b208e0259dfe
xiaoyiyy123 <2916244229@qq.com>
2024-01-19 10:40:45 +0800

    修改震动频率

--- User/sdk_api/hw_config.h
+++ User/sdk_api/hw_config.h
@@ -102,7 +102,7 @@ ________________________________________________________________________________
         #define APP_TRIGGER_MIN_R                   (250)
         #define APP_TRIGGER_CAL_DEADZONE            5
         #define APP_TRIGGER_CAL_SIDE_DEADZONE       14
-        #define HW_PWM_FRENQUENCY                   10000
+        #define HW_PWM_FRENQUENCY                   100
 
         #define HW_MOTOR_ENABLED                   1
         #define JOYSTICK_CAl_END_KEY                (HW_KEY_START | HW_KEY_SELECT)
